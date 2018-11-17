8d97fbb59990deb5ff6537c6b51cc555087501c3
lelanthran <lelanthran@gmail.com>
2018-11-17 09:26:07 +0200

    Added shell script to fetch all the dependencies
    1. This must be modified to switch between yum/apt depending on what is
    detected.

---
+++ Linux/get_deps.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+
+sudo apt-get -y install python-pip
+sudo apt-get -y install python-dev
+sudo apt-get -y install python-qt4
+sudo apt-get -y install python-pyaudio
+sudo apt-get -y install python-setuptools
+sudo apt-get  -y install pychess
+sudo -H pip  install python-chess
+pip install --upgrade pip
+sudo -H pip install psutil
+sudo -H pip install pygal
+sudo -H pip install chardet
+sudo -H pip install Pillow
+sudo -H pip install PhotoHash
+sudo -H pip install Cython
+sudo -H pip install scandir
